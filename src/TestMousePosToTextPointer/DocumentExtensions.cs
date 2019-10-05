8e338a0fef767db64feee929276da6d07e51763c
Behzad <behzad.khosravifar@gmail.com>
2019-10-05 17:18:54 +0330

    split methods to other class

---
+++ src/TestMousePosToTextPointer/DocumentExtensions.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Automation.Peers;
+using System.Windows.Automation.Provider;
+using System.Windows.Automation.Text;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace TestMousePosToTextPointer
+{
+    public static class DocumentExtensions
+    {
+        private static double LineHeight { get; set; } = 25;
+        private static FontFamily ArialFontFamily { get; set; } = new FontFamily("Arial");
+        private static XmlLanguage Lang { get; set; } = XmlLanguage.GetLanguage("fa-IR");
+        private static Visual Visual => Application.Current.MainWindow;
+
+        // Point is specified relative to the given visual
+        public static TextPointer ScreenPointToTextPointer(this FlowDocument document, Point screenPoint)
+        {
+            try
+            {
+                // Get text before point using automation
+                var peer = new DocumentAutomationPeer(document);
+                var textProvider = (ITextProvider)peer.GetPattern(PatternInterface.Text);
+                var rangeProvider = textProvider?.RangeFromPoint(screenPoint);
+                rangeProvider?.MoveEndpointByUnit(TextPatternRangeEndpoint.Start, TextUnit.Document, 1);
+                var charsBeforePoint = rangeProvider?.GetText(int.MaxValue)?.Length ?? 0;
+
+                // Find the pointer that corresponds to the TextPointer
+                var pointer = document.ContentStart.GetPositionAtOffset(charsBeforePoint);
+
+                // Adjust for difference between "text offset" and actual number of characters before pointer
+                for (var i = 0; i < 10; i++)  // Limit to 10 adjustments
+                {
+                    var error = charsBeforePoint - new TextRange(document.ContentStart, pointer).Text.Length;
+                    if (error == 0) break;
+                    pointer = pointer?.GetPositionAtOffset(error);
+                }
+                return pointer;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return null;
+            }
+        }
+
+        public static TextPointer GetPoint(this TextPointer start, int x)
+        {
+            var ret = start;
+            var i = 0;
+            while (i < x)
+            {
+                var b = ret.GetPointerContext(LogicalDirection.Backward);
+                if (b == TextPointerContext.Text || b == TextPointerContext.None)
+                    i++;
+
+                if (ret.GetPositionAtOffset(1, LogicalDirection.Forward) == null)
+                    return ret;
+                else
+                    ret = ret.GetPositionAtOffset(1, LogicalDirection.Forward);
+            }
+
+            return ret;
+        }
+
+        public static List<string> CalcLinesByFirstOfNextLine(this FlowDocument document, string content, double width)
+        {
+            var lines = new List<string>();
+            var offsetRanges = new List<(int, int)>();
+
+            var lineHalfHeight = LineHeight / 2;
+            var offset = 0;
+            var startTextPointer = document.ContentStart;
+            var startPoint = startTextPointer.GetCharacterRect(LogicalDirection.Backward).Location;
+            var xPoint = startPoint.X;
+            var yPoint = startPoint.Y + lineHalfHeight;
+
+            // While we are not at the end of document
+            while (offset < content.Length)
+            {
+                // next line start position
+                yPoint += LineHeight;
+                var nextLineStartPoint = Visual.PointToScreen(new Point(xPoint, yPoint));
+                var nextLineStartTextPointer = document.ScreenPointToTextPointer(nextLineStartPoint);
+
+                var nextOffset = nextLineStartTextPointer != null
+                    ? new TextRange(document.ContentStart, nextLineStartTextPointer).Text.Length
+                    : content.Length;
+
+                if (nextOffset <= offset)
+                    nextOffset = content.Length;
+
+                offsetRanges.Add((offset, nextOffset - 1));
+                var lineText = content.Substring(offset, nextOffset - offset);
+                lines.Add(lineText);
+
+                offset = nextOffset;
+            }
+
+            return lines;
+        }
+
+        public static List<string> CalcLinesByEndOfLine(this FlowDocument document, string content, double width)
+        {
+            var lines = new List<string>();
+            var offsetRanges = new List<(int, int)>();
+
+            var lineHalfHeight = LineHeight / 2;
+            var offset = 0;
+            var startTextPointer = document.ContentStart;
+            var startPoint = startTextPointer.GetCharacterRect(LogicalDirection.Backward).Location;
+            var xPoint = startPoint.X;
+            var yPoint = startPoint.Y + lineHalfHeight;
+
+            // While we are not at the end of document
+            while (offset < content.Length)
+            {
+                // next line start position
+                yPoint += LineHeight;
+                var nextLineStartPoint = Visual.PointToScreen(new Point(xPoint, yPoint));
+                var nextLineStartTextPointer = document.ScreenPointToTextPointer(nextLineStartPoint);
+
+                var nextOffset = nextLineStartTextPointer != null
+                    ? new TextRange(document.ContentStart, nextLineStartTextPointer).Text.Length
+                    : content.Length;
+
+                if (nextOffset <= offset)
+                    nextOffset = content.Length;
+
+                offsetRanges.Add((offset, nextOffset - 1));
+                var lineText = content.Substring(offset, nextOffset - offset);
+                lines.Add(lineText);
+
+                offset = nextOffset;
+            }
+
+            return lines;
+        }
+
+       
+    }
+}
--- src/TestMousePosToTextPointer/MainWindow.xaml.cs
+++ src/TestMousePosToTextPointer/MainWindow.xaml.cs
@@ -21,7 +21,6 @@ namespace TestMousePosToTextPointer
         private FontFamily ArialFontFamily { get; set; } = new FontFamily("Arial");
         private XmlLanguage Lang { get; set; } = XmlLanguage.GetLanguage("fa-IR");
         private string ContentText { get; set; }
-        private List<string> Lines { get; set; } = new List<string>();
 
 
         public MainWindow()
@@ -46,10 +45,10 @@ namespace TestMousePosToTextPointer
             SizeChanged += delegate
             {
                 var width = FlowDocumentFrame.ActualWidth.Equals(double.NaN) ? 555.555 : FlowDocumentFrame.ActualWidth;
-                CalcLines(ContentText, width);
+                var lines = TestFlowPage.CalcLinesByEndOfLine(ContentText, width);
                 ListBoxLines.Items.Clear();
 
-                foreach (var line in Lines)
+                foreach (var line in lines)
                     ListBoxLines.Items.Add(line);
             };
         }
@@ -61,106 +60,6 @@ namespace TestMousePosToTextPointer
             return run?.Text;
         }
 
-
-        private static TextPointer GetPoint(TextPointer start, int x)
-        {
-            var ret = start;
-            var i = 0;
-            while (i < x)
-            {
-                var b = ret.GetPointerContext(LogicalDirection.Backward);
-                if (b == TextPointerContext.Text || b == TextPointerContext.None)
-                    i++;
-
-                if (ret.GetPositionAtOffset(1, LogicalDirection.Forward) == null)
-                    return ret;
-                else
-                    ret = ret.GetPositionAtOffset(1, LogicalDirection.Forward);
-            }
-
-            return ret;
-        }
-
-        List<(int, int)> OffsetRanges = new List<(int, int)>();
-
-        private List<string> CalcLinesByFirstOfNextLine(string content, double width)
-        {
-            Lines.Clear();
-            OffsetRanges.Clear();
-
-            var lineHalfHeight = LineHeight / 2;
-            var offset = 0;
-            var startTextPointer = TestFlowPage.ContentStart;
-            var startPoint = startTextPointer.GetCharacterRect(LogicalDirection.Backward).Location;
-            var xPoint = startPoint.X;
-            var yPoint = startPoint.Y + lineHalfHeight;
-
-            // While we are not at the end of document
-            while (offset < content.Length)
-            {
-                // next line start position
-                yPoint += LineHeight;
-                var nextLineStartPoint = PointToScreen(new Point(xPoint, yPoint));
-                var nextLineStartTextPointer = TestFlowPage.ScreenPointToTextPointer(nextLineStartPoint);
-
-                var nextOffset = nextLineStartTextPointer != null
-                    ? new TextRange(TestFlowPage.ContentStart, nextLineStartTextPointer).Text.Length
-                    : content.Length;
-
-                if (nextOffset <= offset)
-                    nextOffset = content.Length;
-
-                OffsetRanges.Add((offset, nextOffset - 1));
-                var lineText = content.Substring(offset, nextOffset - offset);
-                Lines.Add(lineText);
-
-                offset = nextOffset;
-            }
-
-            return Lines;
-        }
-
-        private List<string> CalcLinesByEndOfLine(string content, double width)
-        {
-            Lines.Clear();
-            OffsetRanges.Clear();
-
-            var lineHalfHeight = LineHeight / 2;
-            var offset = 0;
-            var startTextPointer = TestFlowPage.ContentStart;
-            var startPoint = startTextPointer.GetCharacterRect(LogicalDirection.Backward).Location;
-            var xPoint = startPoint.X;
-            var yPoint = startPoint.Y + lineHalfHeight;
-
-            // While we are not at the end of document
-            while (offset < content.Length)
-            {
-                // next line start position
-                yPoint += LineHeight;
-                var nextLineStartPoint = PointToScreen(new Point(xPoint, yPoint));
-                var nextLineStartTextPointer = TestFlowPage.ScreenPointToTextPointer(nextLineStartPoint);
-
-                var nextOffset = nextLineStartTextPointer != null
-                    ? new TextRange(TestFlowPage.ContentStart, nextLineStartTextPointer).Text.Length
-                    : content.Length;
-
-                if (nextOffset <= offset)
-                    nextOffset = content.Length;
-
-                OffsetRanges.Add((offset, nextOffset - 1));
-                var lineText = ContentText.Substring(offset, nextOffset - offset);
-                Lines.Add(lineText);
-
-                offset = nextOffset;
-            }
-
-            return Lines;
-        }
-
-        private List<string> CalcLines(string content, double width)
-        {
-            return CalcLinesByEndOfLine(content, width);
-        }
 
         private void Test_MouseMove(object sender, MouseEventArgs e)
         {
@@ -187,37 +86,4 @@ namespace TestMousePosToTextPointer
         }
     }
 
-    public static class DocumentExtensions
-    {
-        // Point is specified relative to the given visual
-        public static TextPointer ScreenPointToTextPointer(this FlowDocument document, Point screenPoint)
-        {
-            try
-            {
-                // Get text before point using automation
-                var peer = new DocumentAutomationPeer(document);
-                var textProvider = (ITextProvider)peer.GetPattern(PatternInterface.Text);
-                var rangeProvider = textProvider?.RangeFromPoint(screenPoint);
-                rangeProvider?.MoveEndpointByUnit(TextPatternRangeEndpoint.Start, TextUnit.Document, 1);
-                var charsBeforePoint = rangeProvider?.GetText(int.MaxValue)?.Length ?? 0;
-
-                // Find the pointer that corresponds to the TextPointer
-                var pointer = document.ContentStart.GetPositionAtOffset(charsBeforePoint);
-
-                // Adjust for difference between "text offset" and actual number of characters before pointer
-                for (var i = 0; i < 10; i++)  // Limit to 10 adjustments
-                {
-                    var error = charsBeforePoint - new TextRange(document.ContentStart, pointer).Text.Length;
-                    if (error == 0) break;
-                    pointer = pointer?.GetPositionAtOffset(error);
-                }
-                return pointer;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return null;
-            }
-        }
-    }
 }
