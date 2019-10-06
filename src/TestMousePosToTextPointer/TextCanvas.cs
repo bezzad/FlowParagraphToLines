d8df06b9b4c29e1ea9a47411448f011954c8f60c
behzad khosravifar <behzadkh@hotmail.com>
2019-10-06 13:23:55 +0330

    add textCanvas class to draw text in canvas

--- src/TestMousePosToTextPointer/TextCanvas.cs
+++ src/TestMousePosToTextPointer/TextCanvas.cs
@@ -12,9 +12,14 @@ namespace TestMousePosToTextPointer
         public static readonly DependencyProperty TextProperty;
         // Create a collection of child visual objects.
         private readonly VisualCollection _children;
+        private FormattedText TextFormatter { get; set; }
 
         // Provide a required override for the VisualChildrenCount property.
         protected override int VisualChildrenCount => _children?.Count ?? 0;
+
+        protected Point StartSelectionPoint { get; set; }
+        protected Point EndSelectionPoint { get; set; }
+        protected bool MouseDown { get; set; } 
 
         public string Text
 
@@ -44,6 +49,21 @@ namespace TestMousePosToTextPointer
 
             // Add the event handler for MouseLeftButtonUp.
             MouseLeftButtonUp += MyVisualHost_MouseLeftButtonUp;
+            MouseLeftButtonDown += MyVisualHost_MouseLeftButtonDown;
+        }
+
+        // Capture the mouse event and hit test the coordinate point value against
+        // the child visual objects.
+        private void MyVisualHost_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Retrieve the coordinates of the mouse button event.
+            var pt = e.GetPosition((UIElement)sender);
+            MouseDown = true;
+            StartSelectionPoint = pt;
+            InvalidateVisual();
+
+            // Initiate the hit test by setting up a hit test result callback method.
+            VisualTreeHelper.HitTest(this, null, MyCallback, new PointHitTestParameters(StartSelectionPoint));
         }
 
         // Capture the mouse event and hit test the coordinate point value against
@@ -51,13 +71,38 @@ namespace TestMousePosToTextPointer
         private void MyVisualHost_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
             // Retrieve the coordinates of the mouse button event.
-            var pt = e.GetPosition((UIElement)sender);
+            MouseDown = false;
+            EndSelectionPoint = e.GetPosition((UIElement)sender);
+            InvalidateVisual();
 
             // Initiate the hit test by setting up a hit test result callback method.
-            VisualTreeHelper.HitTest(this, null, MyCallback, new PointHitTestParameters(pt));
+            VisualTreeHelper.HitTest(this, null, MyCallback, new PointHitTestParameters(EndSelectionPoint));
         }
 
+        protected override HitTestResult HitTestCore(PointHitTestParameters hitTestParameters)
+        {
+            if (VisualTreeHelper.GetDescendantBounds(this).Contains(hitTestParameters.HitPoint))
+            {
+                StartSelectionPoint = hitTestParameters.HitPoint;
+                return new PointHitTestResult(this, hitTestParameters.HitPoint);
+            }
 
+            return null;
+
+            //TextFormatter.BuildHighlightGeometry(hitTestParameters.HitPoint, 0, 10);
+            //return new PointHitTestResult(this, hitTestParameters.HitPoint);
+        }
+
+        protected override GeometryHitTestResult HitTestCore(GeometryHitTestParameters hitTestParameters)
+        {
+            // Possibly checking every pixel within the hitTestParameters.HitGeometry.Bounds rectangle
+            var geometry = new RectangleGeometry(VisualTreeHelper.GetDescendantBounds(this));
+            return new GeometryHitTestResult
+                (this, geometry.FillContainsWithDetail(hitTestParameters.HitGeometry));
+
+            //InvalidateVisual();
+            //return base.HitTestCore(hitTestParameters);
+        }
 
         // If a child visual object is hit, toggle its opacity to visually indicate a hit.
         public HitTestResultBehavior MyCallback(HitTestResult result)
@@ -95,7 +140,7 @@ namespace TestMousePosToTextPointer
             var testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
 
             // Create the initial formatted text string.
-            var formattedText = new FormattedText(
+            TextFormatter = new FormattedText(
                 testString,
                 CultureInfo.GetCultureInfo("en-us"),
                 FlowDirection.LeftToRight,
@@ -103,21 +148,20 @@ namespace TestMousePosToTextPointer
                 32,
                 Brushes.Black, 1)
             {
+                // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
                 MaxTextWidth = 500,
                 MaxTextHeight = 540
             };
 
-            // Set a maximum width and height. If the text overflows these values, an ellipsis "..." appears.
-
             // Use a larger font size beginning at the first (zero-based) character and continuing for 5 characters.
             // The font size is calculated in terms of points -- not as device-independent pixels.
-            formattedText.SetFontSize(36 * (96.0 / 72.0), 0, 5);
+            TextFormatter.SetFontSize(36 * (96.0 / 72.0), 0, 5);
 
             // Use a Bold font weight beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetFontWeight(FontWeights.Bold, 6, 11);
+            TextFormatter.SetFontWeight(FontWeights.Bold, 6, 11);
 
             // Use a linear gradient brush beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetForegroundBrush(
+            TextFormatter.SetForegroundBrush(
                 new LinearGradientBrush(
                     Colors.Orange,
                     Colors.Teal,
@@ -125,10 +169,10 @@ namespace TestMousePosToTextPointer
                 6, 11);
 
             // Use an Italic font style beginning at the 28th character and continuing for 28 characters.
-            formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
+            TextFormatter.SetFontStyle(FontStyles.Italic, 28, 28);
 
             // Draw the formatted text string to the DrawingContext of the control.
-            drawingContext.DrawText(formattedText, new Point(0, 0));
+            drawingContext.DrawText(TextFormatter, new Point(0, 0));
 
             //---------------------------------------------------
 
@@ -147,7 +191,7 @@ namespace TestMousePosToTextPointer
             var testString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor";
 
             // Create the initial formatted text string.
-            var formattedText = new FormattedText(
+            TextFormatter = new FormattedText(
                 testString,
                 CultureInfo.GetCultureInfo("en-us"),
                 FlowDirection.LeftToRight,
@@ -163,13 +207,13 @@ namespace TestMousePosToTextPointer
 
             // Use a larger font size beginning at the first (zero-based) character and continuing for 5 characters.
             // The font size is calculated in terms of points -- not as device-independent pixels.
-            formattedText.SetFontSize(36 * (96.0 / 72.0), 0, 5);
+            TextFormatter.SetFontSize(36 * (96.0 / 72.0), 0, 5);
 
             // Use a Bold font weight beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetFontWeight(FontWeights.Bold, 6, 11);
+            TextFormatter.SetFontWeight(FontWeights.Bold, 6, 11);
 
             // Use a linear gradient brush beginning at the 6th character and continuing for 11 characters.
-            formattedText.SetForegroundBrush(
+            TextFormatter.SetForegroundBrush(
                 new LinearGradientBrush(
                     Colors.Orange,
                     Colors.Teal,
@@ -177,11 +221,24 @@ namespace TestMousePosToTextPointer
                 6, 11);
 
             // Use an Italic font style beginning at the 28th character and continuing for 28 characters.
-            formattedText.SetFontStyle(FontStyles.Italic, 28, 28);
+            TextFormatter.SetFontStyle(FontStyles.Italic, 28, 28);
 
             // Draw the formatted text string to the DrawingContext of the control.
-            drawingContext.DrawText(formattedText, new Point(Padding.Left, Padding.Top));
-            drawingContext.DrawRectangle(null, new Pen(Brushes.Blue, 1.0), new Rect(Padding.Left, Padding.Top, formattedText.WidthIncludingTrailingWhitespace, formattedText.Height));
+            drawingContext.DrawText(TextFormatter, new Point(Padding.Left, Padding.Top));
+            if (MouseDown)
+            {
+                // Build the geometry object that represents the text highlight.
+                var textGeometry = TextFormatter.BuildHighlightGeometry(StartSelectionPoint);
+                var textGeometry2 = TextFormatter.BuildGeometry(StartSelectionPoint);
+                
+                var radius = 20;
+                drawingContext.DrawRectangle(null, new Pen(Brushes.Red, 1.0),
+                    new Rect(StartSelectionPoint.X-radius/2, StartSelectionPoint.Y - radius / 2, radius, radius));
+
+                drawingContext.DrawGeometry(Brushes.DarkGray, new Pen(), textGeometry);
+                drawingContext.DrawGeometry(Brushes.Chartreuse, new Pen(), textGeometry2);
+            }
+
             //---------------------------------------------------
         }
     }
