af1a107b6a237aa10f2de8cc28cf0b4d0ab3d654
behzad khosravifar <behzadkh@hotmail.com>
2019-10-08 13:54:34 +0330

    foldering canvas types

--- src/SvgTextViewer/TextCanvas/SelectableTextViewer.cs
+++ src/SvgTextViewer/TextCanvas/SelectableTextViewer.cs
@@ -4,7 +4,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
 
-namespace SvgTextViewer
+namespace SvgTextViewer.TextCanvas
 {
     public class SelectableTextViewer : TextViewer
     {
